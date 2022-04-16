fb1ac7bc399d190b7a454ab2d72bd7f84e5d959d
ComSun <computerhermit96@gmail.com>
2022-04-16 17:15:12 +0900

    #조현진-스프링평가과제/3 ResultReport 구현
    결과를 화면에 표시하는 스프링빈 구현

--- src/main/java/com/nhnacademy/exam/report/ResultReportImpl.java
+++ src/main/java/com/nhnacademy/exam/report/ResultReportImpl.java
@@ -5,9 +5,8 @@ import com.nhnacademy.exam.parser.WaterBill;
 import java.util.Collection;
 
 public class ResultReportImpl implements ResultReport{
-
     @Override
     public void report(Collection<WaterBill> waterBills) {
-
+        System.out.println(waterBills);
     }
 }
